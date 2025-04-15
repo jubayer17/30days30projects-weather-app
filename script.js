0a5488cfbbd816e9dffcbf2a3346ef067c1002af
jubayer17 <jubayer17@cse.pstu.ac.bd>
2025-04-15 22:53:00 +0600

    added humadity and wind features

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+const apiKey = "e6ba33bdfe6e17794e74f2ba76fbdf2a";
+
+async function cheakWeather() {
+  const response = await fetch();
+}
