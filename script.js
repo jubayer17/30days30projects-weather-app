a7677fa55f8b83c550523a7ee0b4044d04c1f421
jubayer17 <jubayer17@cse.pstu.ac.bd>
2025-04-17 23:31:40 +0600

    added code for dynamic weather update

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
-const apiKey = "e6ba33bdfe6e17794e74f2ba76fbdf2a";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "3dd2b7d2ccd3afc176d52851c56cffb9";
 
-async function cheakWeather() {
-  const response = await fetch();
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  try {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    const data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Haze") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    }
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
 }
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value.trim());
+});
